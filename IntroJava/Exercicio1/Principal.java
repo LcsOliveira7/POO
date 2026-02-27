4cac5b6311ae1c3538df6911be01028e0793357e
LcsOliveira7 <rodrigues.lucas3@aluno.ifsp.edu.br>
2026-02-27 16:00:13 -0300

    Delete IntroJava directory

--- IntroJava/Exercicio1/Principal.java
+++
@@ -1,7 +0,0 @@
-public class Principal {
-    public static void main(String[] args) {
-        System.out.println("Hello Wolrd!");
-        System.out.println("Olá Mundo! ");
-        System.out.println("Olá Lucas!");
-    }
-}
