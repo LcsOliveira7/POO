354a777a7cd2e34b2564c87a67908b72179f92cf
LcsOliveira7 <rodrigues.lucas3@aluno.ifsp.edu.br>
2026-02-27 15:58:24 -0300

    Add files via upload

---
+++ IntroJava/Exercicio6/ConversaoTempo.java
@@ -0,0 +1,12 @@
+package Exercicio6;
+
+public class ConversaoTempo {
+    public static void main(String[] args) {
+        int minutos = 130;
+        int horas = 130/60;
+        int minutosConvertidos = 130%60;
+
+        System.out.println(minutos +" minutos correspondem a "+ horas +" hora(s) e "+ minutosConvertidos +" minuto(s)");
+        
+    }
+}
